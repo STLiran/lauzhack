6499ee24282a4416d7756e8b511b40d250eb81f8
David Tang <blueur-ii@hotmail.com>
2016-11-20 03:12:50 +0100

    better code

--- KeyboardPredictor/src/ch/lauzhack/ProbabilitiesDatabase.java
+++ KeyboardPredictor/src/ch/lauzhack/ProbabilitiesDatabase.java
@@ -34,7 +34,7 @@ public class ProbabilitiesDatabase {
 			int read = bufferedReader.read();
 			while (read != -1) {
 				final char c = Character.toLowerCase((char) read);
-				if (alphabet.contains("" + c)) {
+				if ((alphabet + ' ').contains("" + c)) {
 					queue.add(c);
 					if (maxN > 0 && queue.size() > maxN) {
 						queue.remove();
@@ -44,18 +44,10 @@ public class ProbabilitiesDatabase {
 					for (int i = 0; i < n; i++) {
 						addNgram(ngram.substring(i, n));
 					}
-				} else {
 					if (c == ' ') {
-						queue.add(c);
-						if (maxN > 0 && queue.size() > maxN) {
-							queue.remove();
-						}
-						final String ngram = iterableToString(queue);
-						final int n = ngram.length();
-						for (int i = 0; i < n; i++) {
-							addNgram(ngram.substring(i, n));
-						}
+						queue.clear();
 					}
+				} else {
 					queue.clear();
 				}
 
